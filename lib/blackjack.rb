5e58176087ece4bd3e729a18c4c5dbd4f101b9a5
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 04:53:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,6 +7,7 @@ def deal_card
   number = rand(1..11)
   return number
 end
+end
 
 def display_card_total(card_total)
   # code #display_card_total here
@@ -22,17 +23,20 @@ def get_user_input
   # code #get_user_input here
   method = gets.chomp
 end
+end
 
 def end_game(card_total)
   # code #end_game here
   if card_total > 21
     puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  end
 end
 
 def initial_round
   # code #initial_round here
   total = deal_card + deal_card
   display_card_total(total)
+end
 end
 
 def hit?
